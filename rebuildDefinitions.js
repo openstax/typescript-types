f61e1afa5f74185261286ca1169f7cb7434ac999
Philip Schatz <253202+philschatz@users.noreply.github.com>
2018-11-08 06:01:57 -0600

    rebuild definitions when typescript version changes (#1)

---
+++ rebuildDefinitions.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const path = require('path')
+
+const orig = fs.readFileSync(path.join(__dirname, './node_modules/typescript/lib/lib.dom.d.ts'), 'utf-8')
+const converted = orig
+    .replace(/^type/gm, 'export type')
+    .replace(/^interface/gm, 'export interface')
+    .replace(/^declare/gm, 'export')
+
+
+// Do some custom renaming:
+if (converted.search(/^export var URL: \{/gm) <= 0) {
+    throw new Error(`Cannot rename URL to URLConstructor`)
+}
+
+const converted2 = converted.replace(/^export var URL: \{/gm, 'export var URLConstructor: {')
+
+fs.writeFileSync(path.join(__dirname, 'lib.dom.d.ts'), converted2)
