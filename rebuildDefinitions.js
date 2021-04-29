f34bf9daed8010c6b8f61cefafe0f7888b50e00e
jarosik10 <bartosz.jaros96@gmail.com>
2021-04-29 17:04:40 +0200

    Auto DOMParser convertion

--- rebuildDefinitions.js
+++ rebuildDefinitions.js
@@ -13,6 +13,8 @@ if (converted.search(/^export var URL: \{/gm) <= 0) {
     throw new Error(`Cannot rename URL to URLConstructor`)
 }
 
-const converted2 = converted.replace(/^export var URL: \{/gm, 'export var URLConstructor: {')
+const converted2 = converted
+    .replace(/^export var URL: \{/gm, 'export var URLConstructor: {')
+    .replace(/^export var DOMParser: \{/gm, 'export var DOMParserConstructor: {')
 
 fs.writeFileSync(path.join(__dirname, 'lib.dom.d.ts'), converted2)
