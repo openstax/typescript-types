7ccd11f8124eafc0c9483346e18c4eb83d90de7c
jarosik10 <bartosz.jaros96@gmail.com>
2021-06-09 15:32:43 +0200

    Add renaming for resizeObserver

--- rebuildDefinitions.js
+++ rebuildDefinitions.js
@@ -12,5 +12,6 @@ const converted2 = converted
     .replace(/^export var URL: \{/gm, 'export var URLConstructor: {')
     .replace(/^export var DOMParser: \{/gm, 'export var DOMParserConstructor: {')
     .replace(/^export var KeyboardEvent: \{/gm, 'export var KeyboardEventConstructor: {')
+    .replace(/^export var ResizeObserver: \{/gm, 'export var ResizeObserverConstructor: {')
 
 fs.writeFileSync(path.join(__dirname, 'lib.dom.d.ts'), converted2)
